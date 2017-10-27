8bff23843dca708c3c2841fac7e3e74a58a9cb7c
Walter Nelson <wnelson@afg.tech>
2017-10-26 21:29:14 -0700

    Finished

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # SewCodeTest
 SEW Code Test
+
+Approach:
+
+Created a HashSet of all words.
+Cycled through all the words and constructed short words by iterating through the word backwards and then checking to see if they exist in the HashSet.
+
+CONSTRUCTED WORDS RESULTS:
+
+electroencephalographically is 27 characters long and is made up of the following short words:
+        electro
+        en
+        cep
+        ha
+        log
+        rap
+        hic
+        ally
+
+electroencephalographers is 24 characters long and is made up of the following short words:
+        electro
+        en
+        cep
+        ha
+        lo
+        graph
+        ers
+
+Total Constructed Words: 29,918
+
--- SewCodeTest/Program.cs
+++ SewCodeTest/Program.cs
@@ -9,72 +9,82 @@ namespace SewCodeTest
     {
         static void Main(string[] args)
         {
+            // Check if there is an argument
+            if (args.Length != 1)
+            {
+                return;
+            }
+
+            // Check to see if the argument is a file
+            var wordFile = args[0];
+            if (!File.Exists(wordFile))
+            {
+                return;
+            }
+
+            // Find Constructed Words
+            var constructedWords = FindConstructedWords(wordFile);
+
+            // Display Constructed Words Results
+            DisplayConstructedWordsResult(constructedWords);
+        }
+
+        private static List<ConstructedWord> FindConstructedWords(string wordFile)
+        {
+            // Create a list of all words contracted of other words
             var constructedWords = new List<ConstructedWord>();
 
-            var words = new HashSet<string>(File.ReadLines("wordlist.txt"));
+            // Get all the words from the file
+            var words = new HashSet<string>(File.ReadLines(wordFile));
 
+            // Cycle through all the words looking for constructed words
             foreach (var word in words)
             {
                 var constructedWord = new ConstructedWord(word);
-                var shortword = string.Empty;
+                var shortWord = string.Empty;
 
+                // Cycle through all the characters in the word backwards looking for short words
                 for (int i = word.Length - 1; i >= 0; i--)
                 {
-                    shortword = word[i]+ shortword;
-                    if (words.Contains(shortword) && shortword != word)
+                    shortWord = word[i] + shortWord;
+                    if (words.Contains(shortWord) && shortWord != word)
                     {
-                        constructedWord.ShortWords.Add(shortword);
-                        shortword = string.Empty;
+                        constructedWord.ShortWords.Add(shortWord);
+                        shortWord = string.Empty;
                     }
                 }
 
-                //for (int i = 0; i < word.Length; i++)
-                //{
-                //    shortword += word[i];
-                //    if (wordlist.Contains(shortword) && shortword != word)
-                //    {
-                //        constructedWord.ShortWords.Add(shortword);
-                //        if (i <= word.Length)
-                //        {
-                //            shortword = word.Substring(i + 1);
-                //            if (wordlist.Contains(shortword))
-                //            {
-                //                constructedWord.ShortWords.Add(shortword);
-                //                i = word.Length;
-                //            }
-                //        }
-                //        shortword = string.Empty;
-                //    }
-                //}
-
-                //foreach (var chr in word)
-                //{
-                //    shortword += chr;
-                //    if (wordlist.Contains(shortword) && shortword != word)
-                //    {
-                //        constructedWord.ShortWords.Add(shortword);
-                //        shortword = string.Empty;
-                //    }
-                //}
-
-                if (string.IsNullOrEmpty(shortword))
+                // The word is constructed of other words
+                if (string.IsNullOrEmpty(shortWord))
                 {
                     constructedWords.Add(constructedWord);
                 }
             }
 
+            return constructedWords;
+        }
+
+        private static void DisplayConstructedWordsResult(List<ConstructedWord> constructedWords)
+        {
+            // Get the top 2 longest constructed words
             var longestWords = constructedWords.OrderByDescending(c => c.Length).Take(2);
 
+            Console.WriteLine("CONSTRUCTED WORDS RESULTS:");
             foreach (var longestWord in longestWords)
             {
-                Console.WriteLine(longestWord.Word);
+                Console.WriteLine($"\n{longestWord.Word} is {longestWord.Length} characters long and is made up of the following short words:");
+                longestWord.ShortWords.Reverse();
                 foreach (var shortword in longestWord.ShortWords)
                 {
-                    Console.WriteLine(shortword);
+                    Console.WriteLine($"\t{shortword}");
                 }
             }
 
-            Console.ReadLine();
+            Console.WriteLine($"\nTotal Constructed Words: {constructedWords.Count():n0}");
+
+            Console.WriteLine("\nPress any key to continue.");
+
+            Console.ReadKey();
         }
     }
 }
